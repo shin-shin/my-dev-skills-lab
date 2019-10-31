c26394cd9832e20919bdeec46d22908200da6a6d
Irene Tokareva <longhorn09@gmail.com>
2019-10-30 20:07:12 -0500

    fix some minor issues

--- js/main.js
+++ js/main.js
@@ -5,17 +5,12 @@ $( "#new-skill" ).on("submit",function(event) {
     if (newSkill === "") return;
     $("ul").append(`<li><button>x</button><span>${newSkill}</span></li>`);
     $('input[type="text"]').val("");
-    // $(this).val("");
-    console.log("new skill submitted");
-    console.log(`new skill is ${newSkill}`);
 });
 
-let listItem = $("ul").html(`<li><button>x</button><span>${newSkill}</span></li>`)
 
 $("ul").on("click", "button", function(){
     function clear() {$(this).closest("li").remove()}
     $(this).closest("li").fadeOut("fast", clear);
-    console.log($('li').closest("tr"));
-    console.log("list button clicked");})
+})
 
 
