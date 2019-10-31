49626f93e8f000c3e8fc37956d9fefa68bb42d18
Irene Tokareva <longhorn09@gmail.com>
2019-10-30 20:25:39 -0500

    fix submit issue

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 let newSkill;
 
-$( "#new-skill" ).on("submit",function(event) {
+$("#new-skill").on("submit", function (event) {
+    event.preventDefault();
     newSkill = $("input").val();
     if (newSkill === "") return;
     $("ul").append(`<li><button>x</button><span>${newSkill}</span></li>`);
     $('input[type="text"]').val("");
 });
 
-
-$("ul").on("click", "button", function(){
-    function clear() {$(this).closest("li").remove()}
+$("ul").on("click", "button", function () {
+    function clear() { $(this).closest("li").remove() }
     $(this).closest("li").fadeOut("fast", clear);
 })
 
